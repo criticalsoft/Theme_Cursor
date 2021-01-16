3cc0502d658b4478c042758fba8aff74d46b1a93
criticalsoft <soft76842@gmail.com>
2021-01-17 02:28:50 +0900

    INIT

---
+++ Install.sh
@@ -0,0 +1,12 @@
+########Cursor########
+#May Cause All GTK App Icon Not Show Up In Plasma Problem (eg Kvantum QTCurve)
+#ln -sf ~/Icon/ ~/.icons
+
+mkdir -p ~/.icons/
+for i in ./*.tgz; do tar -xvzf $i -C ~/.icons/; done
+
+kcmshell5 kcm_cursortheme
+
+
+
+
